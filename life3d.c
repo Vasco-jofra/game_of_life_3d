75db80855bc1476e81b75edb86f98e8f9b6dfb04
Vasco-jofra <joaovascofranco@hotmail.com>
2017-03-22 18:26:21 +0000

    merged

--- life3d.c
+++ life3d.c
@@ -2,16 +2,86 @@
 #include <stdlib.h>
 #include <string.h>
 
-#define MAX_LINE_SIZE 64
+/********************/
+/*** STRUCTURES ***/
+/********************/
 
-int main(int argc, char** argv) {
-    //printf("%d", atoi(NULL));
-    if(argc != 3) {
+// Node representing elements of sparse matrix
+struct node {
+    int z;
+    struct node *next, *prev;
+};
+
+typedef struct node* z_list;
+
+/********************/
+/*** FUNCTIONS ***/
+/********************/
+
+/**	Initialize hashmap	**/
+void STinit(int max, z_list matrix[][max])
+{
+    int i, j;
+
+    for (i = 0; i < max; i++)
+        for (j = 0; j < max; j++)
+            matrix[i][j] = NULL;
+}
+
+/**  Add alive cell coordinates in hashmap  **/
+// Inserted by ordered to maintain sparse matrix
+void STinsert(int n, z_list matrix[][n], int x, int y, int z)
+{
+    z_list ptr;
+    z_list new_el;
+
+    ptr = matrix[x][y];
+    new_el = (z_list)malloc(sizeof(z_list));
+    new_el->z = z	;
+    new_el->next = NULL;
+    new_el->prev = NULL;
+
+    if (matrix[x][y] == NULL) {
+        matrix[x][y] = new_el;
+        printf("AQUI111: %d %d %d\n", x, y, matrix[x][y]->z);
+    }
+
+    else {
+        if (z < ptr->z) {
+            //matrix[x][y]->prev = new_el;
+            matrix[x][y]->prev = new_el;
+            new_el->next = matrix[x][y];
+            matrix[x][y] = new_el;
+            return;
+        } else {
+            while (ptr->next != NULL) {
+                if (z < ptr->next->z) {
+                    new_el->next = ptr->next;
+                    new_el->prev = ptr;
+                    ptr->next = new_el;
+                    printf("PUMBAS: %d %d %d\n", x, y, matrix[x][y]->z);
+                    return;
+                } else
+                    ptr = ptr->next;
+            }
+            printf("INSERE NO ULTIMO\n");
+            ptr->next = new_el;
+            new_el->prev = ptr;
+        }
+    }
+    printf("FIM\n");
+
+    return;
+}
+
+int main(int argc, char* argv[])
+{
+    if (argc != 3) {
         printf("[ERROR] Incorrect usage!\n");
         printf("[Usage] ./life3d <input_file> <nr_generations>\n");
         return -1;
     }
-    char *input_file  = argv[1];
+    char* input_file = argv[1];
     int generations = atoi(argv[2]);
     printf("Input file: '%s'\nGenerations: %d\n", input_file, generations);
 
@@ -20,46 +90,38 @@ int main(int argc, char** argv) {
         return -1;
     }
 
-    FILE *f = fopen(input_file, "r");
-    if (f == NULL) {
-        printf("[ERROR] Unable to read the input file.\n", generations);
+    FILE* fp = fopen(input_file, "r");
+    if (fp == NULL) {
+        printf("[ERROR] Unable to read the input file.\n");
         perror("[ERROR]");
         return -1;
     }
-
-    char line[MAX_LINE_SIZE];
-    int size;
-
-    // Get size
-    if (fgets(line, MAX_LINE_SIZE, f) == NULL) {
-        printf("[ERROR] Unable to read size from the input file.\n", generations);
+	int SIZE;
+    if (fscanf(fp, "%d", &SIZE) == EOF) {
+		printf("[ERROR] Unable to read the size.\n");
         return -1;
     }
-    size = atoi(line);
-    printf("size: %d\n", size);
 
-    char *_x, *_y, *_z;
-    int x, y, z;
-    while(fgets(line, MAX_LINE_SIZE, f) != NULL) {
-        char *aux = line;
-        // printf("Got: %s", line);
-        _x = strtok(aux , " ");
-        _y = strtok(NULL, " ");
-        _z = strtok(NULL, "\n");
-        if(!_x || !_y || !_z) {
-            printf("x: %p, b: %p, z: %p\n", _x, _y, _z);
-            printf("[ERROR] Found an error parsing the input file.\n", generations);
-            return -1;
-        }
-        x = atoi(_x);
-        y = atoi(_y);
-        z = atoi(_z);
+    int i, j, x, y, z;
+    z_list ptr;
+    z_list cell_matrix[SIZE][SIZE];
 
-        // @TODO: do something here with the values. Populate the matrix.
-        printf("(%d, %d, %d)\n", x, y, z);
+    STinit(SIZE, cell_matrix);
+
+    while (fscanf(fp, "%d %d %d", &x, &y, &z) != EOF) {
+        printf("%d %d %d\n", x, y, z);
+        STinsert(SIZE, cell_matrix, x, y, z);
     }
+    fclose(fp);
 
-    fclose(f);
-
+	// Print
+    for (i = 0; i < SIZE; i++)
+        for (j = 0; j < SIZE; j++) {
+            ptr = cell_matrix[i][j];
+            while (ptr != NULL) {
+                printf("x y z : %d %d %d\n", i, j, ptr->z);
+                ptr = ptr->next;
+            }
+        }
     return 0;
 }
